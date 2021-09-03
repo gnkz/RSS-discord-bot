fed2cc3582daf2fcae8a1e975b8a5e0af5e8b0ce
gnkz <gnkz.dev@gmail.com>
2021-09-02 22:18:59 -0400

    use JSON_RPC_URL env variable

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,9 @@ import { RSP__factory } from './contracts/factories/RSP__factory';
 
 const THIS_CHANNEL_ONLY = process.env.TARGET_CHANNEL;
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
+const JSON_RPC_URL = process.env.JSON_RPC_URL;
 
-const provider = new ethers.providers.JsonRpcProvider('http://192.168.3.10:8545');
+const provider = new ethers.providers.JsonRpcProvider(JSON_RPC_URL);
 const RSS = RSS__factory.connect('0x020bb206cd689d6981182579da490d5f4ceb4c46', provider);
 const RSP = RSP__factory.connect('0xd1cb076f657a38538a5579e3772788e34e83b19c', provider);
 const bot = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
@@ -84,7 +85,7 @@ bot.on('ready', () => {
 });
 
 bot.on('messageCreate', async (msg) => {
-    if (msg.channel.id !== THIS_CHANNEL_ONLY) return;
+    if (THIS_CHANNEL_ONLY !== undefined && msg.channel.id !== THIS_CHANNEL_ONLY) return;
     if (msg.author.bot) return;
 
     try {
